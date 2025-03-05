21ab47cb51e9da6deea6184c9ad34b9b01e6d247
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2025-03-05 09:01:59 +0100

    multithread

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import socket
+import asyncio
 
 from app.parse_request import parse_request
 from app.parse_response import parse_response
 from app.action import action
 
 
-def __process_con(conn: socket.socket):
+async def __process_con(conn: socket.socket):
     contentAsBytes = conn.recv(10000)
 
     content = contentAsBytes.decode("utf-8")
@@ -23,14 +24,12 @@ def __process_con(conn: socket.socket):
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    print("Starting on port 4221!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    (conn, _) = server_socket.accept()  # wait for client
-    __process_con(conn)
+    while True:
+        (conn, _) = server_socket.accept()  # wait for client
+        asyncio.run(__process_con(conn))
 
 
 if __name__ == "__main__":
